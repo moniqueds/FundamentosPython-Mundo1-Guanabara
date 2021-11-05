ee8530ea941ae1f0a82168d9c5d1162d9be0be1e
Monique da Silva <89393449+moniqueds@users.noreply.github.com>
2021-11-05 08:09:04 -0300

    Add files via upload

---
+++ usodecores.py
@@ -0,0 +1,7 @@
+
+print("\033[0;30;41mOlá, Mundo!\033[m")
+print('\033[4;33;44mOlá, Mundo!\033[m')
+print('\033[1;35;43mOlá, Mundo!\033[m')
+print('\033[30;42mOlá, Mundo!\033[m')
+print('\033[mOlá, Mundo!\033[m')
+print('\033[7:30m\033[m')
